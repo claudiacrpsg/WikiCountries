8e85345336c9fe1fdc1a66762a070db29d73f8e4
claudia <claudiacrpsantos@hotmail.com>
2020-02-06 21:49:42 +0100

    description function fixed

--- scripts/wikicountries.js
+++ scripts/wikicountries.js
@@ -3,47 +3,40 @@ let countriesVue = new Vue({
   data: {
     countries: [],
     search: "",
-    show: false,
+    infoShow: false,
+    welcomeShow: true,
     name: "",
-    border:"",
   },
 
   methods: {
-    getData: function () {
+    getData: function () { //fetch function
       fetch('https://gist.githubusercontent.com/maguayo/37bafd58f214e519c74001e6eef10286/raw/aae57688cc9dde956e780d632bfa144f00119a7b/countries.json')
         .then(response => {
           return response.json()
         })
         .then(data => {
           this.countries = data;
-          console.log(this.countries)
         })
         .catch(err => {
           return err;
-
         })
     },
 
-    description: function (des) {
+    description: function (des) { //function that allows the display of information specific for every country
       this.name = des;
       for (var i = 0; i<this.countries.length; i++) {
         if (this.name.match(this.countries[i].name.common)) {
-          console.log(this.name)
-          this.show = true;
+          this.infoShow = true;
+          this.welcomeShow = false;
         }
-        // if(this.border.match(this.countries[i].border)){
-        //   console.log(this.border)
-        // }
       }
     },
-
   },
 
   computed: {
-    filterCountries: function () {
+    filterCountries: function () { //function that allows the user to search for a country
       return this.countries.filter((item) => item.name.common.toLowerCase().match(this.search.toLowerCase()));
     },
-
   },
 
   created() {
